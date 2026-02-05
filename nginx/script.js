21e1a82b1b8f0bf22cd350510e031a3e5fd4b7a4
YongSu Chang <ahae91@gmail.com>
2026-02-05 16:34:41 +0900

    chore: Change request url

--- nginx/script.js
+++ nginx/script.js
@@ -3,7 +3,7 @@ async function fetchData() {
     resultDiv.innerText = "데이터를 불러오는 중...";
     try {
         // Nginx 리버스 프록시를 통해 /api/ 경로로 요청
-        const response = await fetch('/api/');
+        const response = await fetch('/db-test/');
         const data = await response.json();
         resultDiv.innerText = JSON.stringify(data, null, 2);
     } catch (error) {
